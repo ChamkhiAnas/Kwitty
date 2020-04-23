36505bf269d9f5f222d1a8421621e522d83eb94e
ChamkhiAnas <mahmoudbennouna@gmail.com>
2020-04-23 17:09:14 +0000

    Articles

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Article;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,7 +19,19 @@ Route::get('/', function () {
 });
 
 Route::get('about', function () {
-    return view('about');
+
+    // $article= App\Article::take(2)->get(); //Taking just two last articles
+    // $article= App\Article::paginate(2); //Taking just two last articles
+    // $article= App\Article::latest(2)->get(); //Taking just two last articles by created at date
+
+
+     $articles= App\Article::all();
+
+
+
+    return view('about', [
+        'articles' => $articles
+    ]);
 });
 
 Route::resource('/posts',"PostsController");
